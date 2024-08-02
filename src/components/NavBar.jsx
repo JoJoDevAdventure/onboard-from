3d51dde3c6b819355ee1641f0a05248ffdc430b2
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-08-02 10:51:01 +0100

    update UI

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => (
   <nav className="bg-transparent py-6 md:px-40 px-8 flex justify-between items-center">
     <img src='./logo.png' className="text-xl font-bold w-40"/>
     <div className="text-[#F6D31F] text-xl hidden md:block uppercase">Designers Onboarding Program</div>
-    <CustomButton text={"ContactUs"} className="  py-3 px-5 rounded text-sm"/>
+    <CustomButton text={"Contact Us"} className="  py-3 px-5 rounded text-sm"/>
   </nav>
 );
 
